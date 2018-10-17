05d0035b0647f96041c51d85abbcacbf78fde54b
A Fakhrurozi S <haifahrul@gmail.com>
2018-10-18 01:03:09 +0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
+    <p> Hello World </p>
+  </div>
+);
+
+export default Index;
