587601f81187adae00e22f9b8fdcb80f8028c815
A Fakhrurozi S <haifahrul@gmail.com>
2018-10-18 01:06:54 +0700

    Link With a Button

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ const Index = () => (
     <Link href="/about">
       <a style={{ fontSize: 20 }}>About Page</a>
     </Link>
+    <Link href="/about">
+      <button>Go to About Page</button>
+    </Link>
     <p> Hello World </p>
   </div>
 );
